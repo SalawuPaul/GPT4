f5539622763e1b9d98416be2fc6c4e8105663406
salawu paul <psalawu@gmail.com>
2024-08-28 10:17:34 +0100

    Add project files.

---
+++ GPT4/Models/ChatMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GPT4.Models
+{
+    public class ChatMessages
+    {
+
+        public string userMessage { get; set; }
+        public string BotMessage { get; set; }
+        public DateTime MessageTimeStamp { get; set; } = DateTime.UtcNow;
+
+    }
+}
---
+++ GPT4/Services/OpenAI/IOpenAIService.cs
@@ -0,0 +1,13 @@
+﻿using GPT4.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GPT4.Services.OpenAI
+{
+    public interface IOpenAIService
+    {
+
+        public Task<ChatMessages> ChatCompletionAsync(string message, List<ChatMessages> chatMessages);
+        Task<ChatMessages> ChatCompletionAsync(string text);
+    }
+}
